3fbf87b28ebbe846e5a38bbfbb0f02b280f80d65
Yuri Fedoseev <y.fedoseev@innopolis.ru>
2016-07-23 21:58:54 +0300

    some inits

--- src/mapper.py
+++ src/mapper.py
@@ -3,9 +3,11 @@ class Mapper:
         pass
 
     # map data by applying some data function
+    # task_id - unique task_id
     # chunk_path - DFS path to the chunk file to map
     # map_script - DFS path to script of map function
-    def map(self, chunk_path, map_script):
+    # reducers - list of all reducers with addresses
+    def map(self, task_id, reducers, chunk_path, map_script):
         return "ok"
 
     def send_to_reducer(self, data, reducer):
--- src/reducer.py
+++ src/reducer.py
@@ -1,2 +1,18 @@
 class Reducer:
-    pass
+    def __init__(self):
+        pass
+
+    # saves mapped key-values data from mapper
+    # mapper_id - unique mapper id
+    # task_id - unique task id
+    # data should be saved in task_id folder
+    def save_mapped_data(self, mapper_id, task_id, data):
+        pass
+
+    # signal from JT for starting reducing
+    def reduce(self, task_id):
+        pass
+
+    # saves reduced result to dfs
+    def save_result_to_dfs(self, task_id, result):
+        pass
